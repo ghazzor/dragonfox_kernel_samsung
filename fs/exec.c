f64accf3cb8afeb70e5c08fcefb6c3d4bedb216b
Harder258 <jorge.erc007@gmail.com>
2023-08-17 09:30:44 +0000

    DragonFox: Introduce KernelSU

--- fs/exec.c
+++ fs/exec.c
@@ -1825,6 +1825,9 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+// KernelSU hook
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+          void *envp, int *flags);
 /*
  * sys_execve() executes a new program.
  */
@@ -1839,6 +1842,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct files_struct *displaced;
 	int retval;
 
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
 
