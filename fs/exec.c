1146d9d09ea94207d68d9831921996dd3427e4b7
Harder258 <jorge.erc007@gmail.com>
2023-08-21 16:26:56 +0000

    DragonFox: Fix build without KernelSU

--- fs/exec.c
+++ fs/exec.c
@@ -1825,9 +1825,11 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+#ifdef CONFIG_KSU
 // KernelSU hook
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
           void *envp, int *flags);
+#endif
 /*
  * sys_execve() executes a new program.
  */
@@ -1842,7 +1844,9 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct files_struct *displaced;
 	int retval;
 
+#ifdef CONFIG_KSU
 	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+#endif
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
 
